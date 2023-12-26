f6572f1396dcb34eb81370a278448f8f1cae4a4b
Jannis <jannispj@gmail.com>
2023-12-26 20:11:22 +0100

    m

---
+++ help.sh
@@ -0,0 +1,8 @@
+echo -e "\e[0mHelp: \e[3;33mman \e[34m*command*"
+echo -e "\e[0mMake executable: \e[3;33mchmod +x \e[34m*file*"
+echo -e "\e[0mSystem update after long time: \e[3;33mpacman -Sy archlinux-keyring && pacman -Su"
+#echo -e "\e[39mWhen failed to commit transaction: \e[33mfind /var/cache/pacman/pkg/ -iname \"*.part\" -delete"
+echo -e "\e[0mShow hidden files: \e[3;33mls -a"
+echo -e "\e[0mShow file properties: \e[3;33mls -l"
+echo -e "\e[0mExecute commands in shell: \e[3;33msource \e[34m*file*"
+echo -e "\e[0m"
